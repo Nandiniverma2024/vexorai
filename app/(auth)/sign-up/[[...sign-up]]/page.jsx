4d5a925ecd0a6cfff69130a7eaedbd2d4770499e
Nandini <nandiniverma202021@gmail.com>
2025-03-12 17:57:17 +0530

    update sign-up form

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -5,8 +5,3 @@ const Page = () => {
 }
 
 export default Page
-
-// postgres code
-// postgresql://neondb_owner:npg_9YDclMrPATj7@ep-fragrant-cherry-a8cx0jg2-pooler.eastus2.azure.neon.tech/neondb?sslmode=require
-
-// Nandini Verma role -> Password -> (npg_CKnagRZ43DzI)
