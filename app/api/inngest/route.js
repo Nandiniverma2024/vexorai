a3ba3c42938d9c675e76b83cbce007bd0170c3cf
Nandini <nandiniverma202021@gmail.com>
2025-03-13 10:07:04 +0530

    resolving error

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -5,8 +5,5 @@ import { generateIndustryInsights } from "@/lib/inngest/functions";
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-    generateIndustryInsights,
-  ],
+  functions: [generateIndustryInsights],
 });
